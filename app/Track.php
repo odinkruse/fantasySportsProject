d9730b36541baf9a532117679951fdd8945924e6
Odin Kruse <odin@octech.io>
2018-02-24 09:35:23 -0800

    no message

--- app/Track.php
+++ app/Track.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
+    protected $fillable = ['name'];
     public function races()
     {
         return $this->hasMany('Race');
